f2b2951411ffb06b94ed5587d0873d5c10eb1200
All_right_Cool <ehrud3220@naver.com>
2025-01-25 00:52:02 +0900

    refact: user/infra/repository/user_repo.py 수정(db 세션 연결 설정)

--- user/infra/repository/user_repo.py
+++ user/infra/repository/user_repo.py
@@ -1,5 +1,25 @@
 # user/infra/repository/user_repo.py
 from user.domain.repository.user_repo import IUserRepository
+from database import SessionLocal
+from user.domain.user import User as UserV0
+from user.infra.db_models.user import User
 
 class UserRepository(IUserRepository):
-    pass
+    def save(self, user: UserV0):
+        new_user = User(
+            id=user.id,
+            profile=user.profile,
+            password=user.password,
+            created_at=user.created_at,
+            updated_at=user.updated_at,
+        )
+        with SessionLocal() as db:
+            try:
+                db = SessionLocal()
+                db.add(new_user)
+                db.commit()
+            finally:
+                db.close()
+    
+    def find_by_email(self, email: str) -> User:
+        pass
