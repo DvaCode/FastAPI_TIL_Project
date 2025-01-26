2718e1e0bbd7c9f970a927762187fc5149c33247
All_right_Cool <ehrud3220@naver.com>
2025-01-26 17:04:05 +0900

    fix: containers.py 오타 수정

--- containers.py
+++ containers.py
@@ -1,10 +1,10 @@
-# conatiners.py
+# containers.py
 
 from dependency_injector import containers, providers
 from user.infra.repository.user_repo import UserRepository
 from user.application.user_service import UserService
 
-class Conatiner(containers.DeclarativeContainer):
+class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
         packages=["user"],
     )
