eb206dd6c74efb61887b3b6978a5d0d5df1ec3cc
All_right_Cool <ehrud3220@naver.com>
2025-05-29 04:33:31 +0900

    Add : config using dotenv

---
+++ config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+    database_username: str
+    database_password: str
+    jwt_secret: str
+
+@lru_cache # 페이지 교체 알고리즘 중 하나 LRU(least recently used) Algorithm
+def get_settings():
+    return Settings()
--- database.py
+++ database.py
@@ -2,8 +2,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import get_settings
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test@localhost:3306/fastapi-ca"
+settings = get_settings()
+SQLALCHEMY_DATABASE_URL = (
+    "mysql+pymysql://"f"{settings.database_username}:{settings.database_password}"
+    "@localhost:3306/fastapi-ca"
+)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
